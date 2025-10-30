07e5084dfd01a8d18903d91e8f77367ff3632098
KYUUBI-K <sasha06609@gmail.com>
2025-10-30 22:00:11 +0200

    add transition and  scroll function

--- js/header.js
+++ js/header.js
@@ -5,3 +5,16 @@ const appHeight = () => {
 
 window.addEventListener("resize", appHeight);
 appHeight();
+
+const header = document.querySelector(".header");
+const header_container = document.querySelector(".header__container");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 20) {
+    header.classList.add("header-scroll");
+    header_container.classList.add("header-container-bg");
+  } else {
+    header.classList.remove("header-scroll");
+    header_container.classList.remove("header-container-bg");
+  }
+});
